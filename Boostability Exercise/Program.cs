657e0d1c7a524c0b47a34322a0ad9be140f6d54b
Dylan Benedict <Dylpickle912@yahoo.com>
2022-03-21 13:58:26 -0400

    Restarted without using actual user input

--- Boostability Exercise/Program.cs
+++ Boostability Exercise/Program.cs
@@ -7,59 +7,17 @@ namespace Boostability_Exercise
 	{
 		static void Main(string[] args)
 		{
-			// Add routes to Home
-			List<string> HomeRoutes = new List<string>();
-			HomeRoutes.Add("/home");
-			HomeRoutes.Add("/privacy");
-			HomeRoutes.Add("/error");
-
-			// Add routes to About
-			List<string> AboutRoutes = new List<string>();
-			HomeRoutes.Add("/about");
-			HomeRoutes.Add("/members");
-			HomeRoutes.Add("/sponsors");
-
-			// Add routes to Event
-			List<string> EventRoutes = new List<string>();
-			HomeRoutes.Add("/events");
-			HomeRoutes.Add("/guestlist");
-
-			foreach (var route in HomeRoutes)
+			string[] routes =
 			{
-				Console.WriteLine(route);
-			}
+				"/home",
+				"/privacy -> /about -> /members",
+				"/members -> /about",
+				"/events -> /guestlist -> /home",
+				"/about -> /members",
+				"/members -> /about"
+			};
 
-			foreach (var route in AboutRoutes)
-			{
-				Console.WriteLine(route);
-			}
 
-			foreach (var route in EventRoutes)
-			{
-				Console.WriteLine(route);
-			}
-
-			// Delegate route delimiter
-			Console.WriteLine("\nEnter a route, separated by ' -> ':");
-			
-			// Get user input
-			string inputRoute = Console.ReadLine();
-
-			Console.WriteLine("\nSubmitted Route: ");
-
-			// Display user input
-			Console.WriteLine(inputRoute);
-
-			// Split route based on delimiter
-			string[] routes = inputRoute.Split(" -> ");
-
-			Console.WriteLine("\nAll Routes:\n");
-
-			// Display all routes as array
-			foreach (var route in routes)
-			{
-				Console.WriteLine(route.Trim() + "\n");
-			}
 		}
 	}
 }
